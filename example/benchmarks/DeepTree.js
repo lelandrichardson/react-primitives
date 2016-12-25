ac4e332567057fad836fa4de8d728097bb2f79b2
Leland Richardson <leland.m.richardson@gmail.com>
2016-12-25 07:38:10 -0800

    Include RNW comparison in benchmarks

--- example/benchmarks/DeepTree.js
+++ example/benchmarks/DeepTree.js
@@ -1,84 +1,87 @@
 import React, { PropTypes } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-primitives';
 
-const propTypes = {
-  depth: PropTypes.number.isRequired,
-  breadth: PropTypes.number.isRequired,
-  wrap: PropTypes.number.isRequired,
-  id: PropTypes.number.isRequired,
+const makeDeepTree = ({ View, StyleSheet }) => {
+  const propTypes = {
+    depth: PropTypes.number.isRequired,
+    breadth: PropTypes.number.isRequired,
+    wrap: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+  };
+
+  class DeepTree extends React.Component {
+    render() {
+      const { depth, breadth, wrap, id } = this.props;
+      let result = (
+        <View
+          style={[
+            styles.outer,
+            depth % 2 === 0 ? styles.even : styles.odd,
+            styles[`custom${id % 3}`],
+          ]}
+        >
+          {depth === 0 && (
+            <View
+              style={[
+                styles.terminal,
+                styles[`terminal${id % 3}`],
+              ]}
+            />
+          )}
+          {depth !== 0 && Array.from({ length: breadth }).map((el, i) => (
+            <DeepTree
+              key={i}
+              wrap={wrap}
+              depth={depth - 1}
+              id={i}
+              breadth={breadth}
+            />
+          ))}
+        </View>
+      );
+      for (var i = 0; i < wrap; i++) {
+        result = <View>{result}</View>;
+      }
+      return result;
+    }
+  }
+
+  DeepTree.propTypes = propTypes;
+
+  const styles = StyleSheet.create({
+    outer: {
+      padding: 4,
+    },
+    odd: {
+      flexDirection: 'row',
+    },
+    even: {
+      flexDirection: 'column',
+    },
+    custom0: {
+      backgroundColor: '#222',
+    },
+    custom1: {
+      backgroundColor: '#666',
+    },
+    custom2: {
+      backgroundColor: '#999',
+    },
+    terminal: {
+      width: 20,
+      height: 20,
+    },
+    terminal0: {
+      backgroundColor: 'blue',
+    },
+    terminal1: {
+      backgroundColor: 'orange',
+    },
+    terminal2: {
+      backgroundColor: 'red',
+    },
+  });
+
+  return DeepTree;
 };
 
-class DeepTree extends React.Component {
-  render() {
-    const { depth, breadth, wrap, id } = this.props;
-    let result = (
-      <View
-        style={[
-          styles.outer,
-          depth % 2 === 0 ? styles.even : styles.odd,
-          styles[`custom${id % 3}`],
-        ]}
-      >
-        {depth === 0 && (
-          <View
-            style={[
-              styles.terminal,
-              styles[`terminal${id % 3}`],
-            ]}
-          />
-        )}
-        {depth !== 0 && Array.from({ length: breadth }).map((el, i) => (
-          <DeepTree
-            key={i}
-            wrap={wrap}
-            depth={depth - 1}
-            id={i}
-            breadth={breadth}
-          />
-        ))}
-      </View>
-    );
-    for (var i = 0; i < wrap; i++) {
-      result = <View>{result}</View>;
-    }
-    return result;
-  }
-}
-
-DeepTree.propTypes = propTypes;
-
-const styles = StyleSheet.create({
-  outer: {
-    padding: 4,
-  },
-  odd: {
-    flexDirection: 'row',
-  },
-  even: {
-    flexDirection: 'column',
-  },
-  custom0: {
-    backgroundColor: '#222',
-  },
-  custom1: {
-    backgroundColor: '#666',
-  },
-  custom2: {
-    backgroundColor: '#999',
-  },
-  terminal: {
-    width: 20,
-    height: 20,
-  },
-  terminal0: {
-    backgroundColor: 'blue',
-  },
-  terminal1: {
-    backgroundColor: 'orange',
-  },
-  terminal2: {
-    backgroundColor: 'red',
-  },
-});
-
-module.exports = DeepTree;
+module.exports = makeDeepTree;
--- example/web/perf.js
+++ example/web/perf.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DeepTree from '../benchmarks/DeepTree';
+import makeDeepTree from '../benchmarks/DeepTree';
 import benchmark from '../benchmarks/benchmark';
 
 const node = document.querySelector('.root');
 const setup = () => {/* */};
 const teardown = () => ReactDOM.unmountComponentAtNode(node);
 
-const deepTree = ({ wrap, depth, breadth, runs }) => () => benchmark({
-  name: `DeepTree (depth=${depth}, breadth=${breadth}, wrap=${wrap})`,
+const ReactPrimitives = require('react-primitives');
+const ReactNativeWeb = require('react-native-web');
+
+const DeepTreeRP = makeDeepTree(ReactPrimitives);
+const DeepTreeRNW = makeDeepTree(ReactNativeWeb);
+
+const deepTreeRP = ({ wrap, depth, breadth, runs }) => () => benchmark({
+  name: `DeepTree (React Primitives) (depth=${depth}, breadth=${breadth}, wrap=${wrap})`,
   runs,
   setup,
   teardown,
   task: () => ReactDOM.render(
-    <DeepTree
+    <DeepTreeRP
+      wrap={wrap}
+      depth={depth}
+      breadth={breadth}
+      id={0}
+    />,
+    node
+  ),
+});
+
+const deepTreeRNW = ({ wrap, depth, breadth, runs }) => () => benchmark({
+  name: `DeepTree (React Native Web) (depth=${depth}, breadth=${breadth}, wrap=${wrap})`,
+  runs,
+  setup,
+  teardown,
+  task: () => ReactDOM.render(
+    <DeepTreeRNW
       wrap={wrap}
       depth={depth}
       breadth={breadth}
@@ -24,16 +46,18 @@ const deepTree = ({ wrap, depth, breadth, runs }) => () => benchmark({
 });
 
 Promise.resolve()
-  .then(deepTree({ wrap: 0, depth: 3, breadth: 3, runs: 100 }))
-  .then(deepTree({ wrap: 4, depth: 3, breadth: 10, runs: 10 }))
-  .then(deepTree({ wrap: 1, depth: 3, breadth: 3, runs: 10 }))
-  .then(deepTree({ wrap: 2, depth: 3, breadth: 3, runs: 10 }))
-  .then(deepTree({ wrap: 3, depth: 3, breadth: 3, runs: 10 }))
-  .then(deepTree({ wrap: 4, depth: 3, breadth: 3, runs: 10 }))
-  .then(deepTree({ wrap: 5, depth: 3, breadth: 3, runs: 10 }))
-  .then(deepTree({ wrap: 6, depth: 3, breadth: 3, runs: 10 }))
+  // .then(deepTreeRP({ wrap: 0, depth: 3, breadth: 3, runs: 100 }))
+  .then(deepTreeRP({ wrap: 4, depth: 3, breadth: 10, runs: 10 }))
+  .then(deepTreeRNW({ wrap: 4, depth: 3, breadth: 10, runs: 10 }))
+  .then(deepTreeRP({ wrap: 1, depth: 5, breadth: 3, runs: 10 }))
+  .then(deepTreeRNW({ wrap: 1, depth: 5, breadth: 3, runs: 10 }))
+  // .then(deepTreeRP({ wrap: 2, depth: 3, breadth: 3, runs: 10 }))
+  // .then(deepTreeRP({ wrap: 3, depth: 3, breadth: 3, runs: 10 }))
+  // .then(deepTreeRP({ wrap: 4, depth: 3, breadth: 3, runs: 10 }))
+  // .then(deepTreeRP({ wrap: 5, depth: 3, breadth: 3, runs: 10 }))
+  // .then(deepTreeRP({ wrap: 6, depth: 3, breadth: 3, runs: 10 }))
   .then(() => ReactDOM.render(
-    <DeepTree
+    <DeepTreeRP
       wrap={0}
       depth={4}
       breadth={3}
