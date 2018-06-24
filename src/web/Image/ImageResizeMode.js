d229b4f4fef6078244657db314e105874137a83c
mathieudutour <mathieu@dutour.me>
2018-06-24 13:33:59 -0600

    remove unused web implementation

--- src/web/Image/ImageResizeMode.js
+++
@@ -1,8 +0,0 @@
-const ImageResizeMode = {
-  contain: 'CONTAIN',
-  cover: 'COVER',
-  none: 'NONE',
-  stretch: 'STRETCH',
-};
-
-module.exports = ImageResizeMode;
--- src/web/StyleSheet/hyphenate.js
+++
@@ -1,4 +0,0 @@
-// Taken from:
-// https://github.com/necolas/react-native-web/blob/master/src/apis/StyleSheet/hyphenate.js
-
-module.exports = s => s.replace(/([A-Z])/g, '-$1').toLowerCase().replace(/^ms-/, '-ms-');
