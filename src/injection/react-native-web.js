f2606e8bc542c86ab83c344b8e841e1a32ddc8cb
Richard Willis <willis.rh@gmail.com>
2022-11-29 06:55:45 +0000

    Support RNW 0.18, remove StyleSheet.resolve API. Refs #147

--- src/injection/react-native-web.js
+++ src/injection/react-native-web.js
@@ -11,30 +11,13 @@ const {
   Easing,
 } = require('react-native-web');
 
-// TODO: figure out a more appropriate way to get StyleSheet.resolve, or potentially remove the
-// API alltogether.
-function getDefault(m) {
-  return m.__esModule === true ? m.default : m;
-}
-
-const StyleRegistry = getDefault(require('react-native-web/dist/cjs/exports/StyleSheet/createStyleResolver'))();
-
-const emptyObject = {};
-
-const resolve = style => {
-  return StyleRegistry.resolve(style) || emptyObject;
-};
-
 ReactPrimitives.inject({
   View,
   Text,
   Image,
   Easing,
   Animated,
-  StyleSheet: {
-    ...StyleSheet,
-    resolve,
-  },
+  StyleSheet,
   Platform: {
     OS: Platform.OS,
     Version: Platform.Version,
